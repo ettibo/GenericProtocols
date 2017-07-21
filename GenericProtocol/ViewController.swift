1960b45f4776013017d28c1fe88a42ab78a3eb6a
Thibaud Huchon <external.thibaud.huchon@allianz.com>
2017-07-21 14:10:59 +0200

    Initial Commit

---
+++ GenericProtocol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenericProtocol
+//
+//  Created by Tibo on 21/07/2017.
+//  Copyright © 2017 Tibo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
