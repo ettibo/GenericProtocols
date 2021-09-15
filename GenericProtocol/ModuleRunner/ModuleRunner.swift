0cd0796466ddc1ece203e726fa8c263dec935a2a
Thibaud HUCHON <t.huchon@LGOP2716.local>
2021-09-15 17:29:58 +0200

    refactoring of useModules method

--- GenericProtocol/ModuleRunner/ModuleRunner.swift
+++ GenericProtocol/ModuleRunner/ModuleRunner.swift
@@ -13,16 +13,22 @@ class ModuleRunner {
         for module in modules {
             if let actions = module.actions {
                 for action in actions {
-                    if let method = action.method {
-                        let selector = NSSelectorFromString(method)
-                        if let obj = module.realObject, obj.responds(to: selector) {
-                            if let value = action.value {
-                                _ = obj.perform(selector, with: value)
-                            } else {
-                                _ = obj.perform(selector)
-                            }
-                        }
+                    if let object = module.realObject {
+                        performAction(action: action, on: object)
                     }
+                }
+            }
+        }
+    }
+    
+    private func performAction(action: GenericAction, on object: AnyObject) {
+        if let method = action.method {
+            let selector = NSSelectorFromString(method)
+            if object.responds(to: selector) {
+                if let value = action.value {
+                    _ = object.perform(selector, with: value)
+                } else {
+                    _ = object.perform(selector)
                 }
             }
         }
